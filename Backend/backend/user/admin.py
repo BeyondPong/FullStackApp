606130cc728d7476513126d9f8a8b7c24a692743
Misung Kim <misung178@gmail.com>
2024-07-10 17:06:25 +0900

    init

--- Backend/backend/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Member, Friend
-from django.contrib.auth.models import Group
-
-admin.site.register(Friend)
-admin.site.register(Member)
-admin.site.unregister(Group)
