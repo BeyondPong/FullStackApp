e3d3601814fb6445ae022f2e6ecab913003c4c5e
Hosung99 <shson1217@kw.ac.kr>
2024-08-03 16:56:48 +0900

    fix: default문법 오류 수정

--- ft_transcendence/frontend/static/js/utility/webSocketFocus.js
+++ ft_transcendence/frontend/static/js/utility/webSocketFocus.js
@@ -1,9 +1,9 @@
 import WebSocketManager from "../state/WebSocketManager.js";
 
-export default function webSocketFocus() {
-    WebSocketManager.isGameSocketFocus = true;
+export function webSocketFocus() {
+  WebSocketManager.isGameSocketFocus = true;
 }
 
-export default function webSocketBlur() {
-    WebSocketManager.isGameSocketFocus = false;
+export function webSocketBlur() {
+  WebSocketManager.isGameSocketFocus = false;
 }
